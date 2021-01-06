a196f1cb092defb2d93bf3bf408db55eaa6850fd
Diana Rybikov <diana.ryb214@gmail.com>
2021-01-06 15:03:02 -0700

    Added to Cli

---
+++ lib/Cli.rb
@@ -0,0 +1,17 @@
+class Cli
+
+    def greeting
+        puts "Welcome to Hogwards School of Witchcraft and Wizardry!"
+
+
+        puts "Whats your name?"
+        name = gets.chomp
+
+        puts "Are you a new student?(Y/N)"
+        student = gets.chomp
+    end
+
+
+
+
+end
