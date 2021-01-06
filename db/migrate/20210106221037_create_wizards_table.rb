3252555a8f6f4eb5b498c19aa240841e871875f4
Nathan <n.gusky88@gmail.com>
2021-01-06 15:32:29 -0700

    migrtions and schema of table

--- db/migrate/20210106221037_create_wizards_table.rb
+++
@@ -1,4 +0,0 @@
-class CreateWizardsTable < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
---
+++ db/migrate/20210106222517_create_wizards_table.rb
@@ -0,0 +1,9 @@
+class CreateWizardsTable < ActiveRecord::Migration[6.1]
+  def change
+   create_table :wizards do |t|
+      t.string :name
+      t.integer :age
+      t.string :DOB
+   end
+  end
+end
